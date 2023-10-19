2f3976d45bde8f6d0a0abc3212dc2333439963d4
Dylan Vu <dylanvu9@gmail.com>
2023-10-18 18:34:28 -0700

    initial copy of the results page

--- lib/device_search_result.dart
+++ lib/device_search_result.dart
@@ -1,3 +1,48 @@
 /* Page to show after looking for device */
 
 // The hardware team has learned some software
+
+import 'package:flutter/material.dart';
+
+class DevicePairingResultPage extends StatefulWidget {
+  const DevicePairingResultPage(
+      {Key? key, required this.title, required this.result})
+      : super(key: key);
+
+  final String title;
+  final bool result;
+
+  @override
+  State<DevicePairingResultPage> createState() =>
+      _DevicePairingResultPageState();
+}
+
+class _DevicePairingResultPageState extends State<DevicePairingResultPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+                width: 150,
+                height: 150,
+                child: CircularProgressIndicator(value: null)),
+            TextButton(
+              onPressed: () {},
+              child: const Text("DEBUG: Go to success page"),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text("DEBUG: Go to failure page"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
