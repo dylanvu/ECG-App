7130ebce85ca34491fde1c39e0e86b496086023a
Katherine Hsu <2katherinehsu@gmail.com>
2023-10-18 18:15:23 -0700

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ECG App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-
         colorScheme: ColorScheme.fromSwatch().copyWith(
           primary: const Color.fromARGB(255, 255, 145, 145),
+          secondary: const Color.fromARGB(255, 120, 148, 230),
+          background: const Color.fromARGB(255, 32, 32, 32),
         ), // Color Scheme
       ),
       home: const MyHomePage(title: 'ECG Home Page'),
@@ -27,15 +27,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -47,17 +38,6 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   bool switch_pressed = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   void _switchClicked(bool value){
     setState(() {
       switch_pressed = false;
@@ -66,53 +46,33 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'ECG READING',
+              style: TextStyle(fontSize: 30),
             ),
             Container(
               decoration: BoxDecoration(
-                color: const Color(0xff7c94b6),
-                border: Border.all(
-                  width: 15,
-                ),
+                color: const Color.fromARGB(255, 240, 198, 198),
                 borderRadius: BorderRadius.circular(2),
                 shape: BoxShape.rectangle,
               ),
-              width: 200,
-              height: 100,
-              child: Text('No Device Found...'),
+              width: 350,
+              height: 150,
+              child: Center(
+                child: Text(
+                  'No Device Found...', 
+                  style: TextStyle(fontSize: 30, color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ),
             Switch(
               activeColor: Colors.pink.shade50,
@@ -130,7 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               );
             },
-            child: Text('Pair with ECG Device'),
+            child: Text('Pair with ECG Device', style: TextStyle(fontSize: 20),),
             ),
           ],
         ),
